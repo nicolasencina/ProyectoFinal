868495c268685a69e1001e4b2da8e629244a9760
Nico <nico.encina2012@gmail.com>
2017-07-04 02:19:54 -0400

    Findind error in chisquare function

--- main2.m
+++ main2.m
@@ -2,9 +2,12 @@ clear all
 filenames = {'features1','features2','features3','features4'};
 
 % Extraccion de datos de base de datos usando metodo 1
-method = 1;
+method = 2;
 database = load(filenames{method});
 database_1 = database.database_features;
+
+database2 = load(filenames{1});
+database_2 = database2.database_features;
 
 % Abrir imagenes de la carpeta de consulta
 pc_name = 'Nico';
@@ -15,7 +18,6 @@ image_database = 'img_database';
 database_info = images_info(pc_name, folder, image_database); 
 query_info = images_info(pc_name, folder, query_database);
 
-query_images = direcciones(pc_name, folder , query_database);
 n_query = length(query_info);
 
 

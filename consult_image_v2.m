e0e5a2fb89e98fbd657ab6dc0c3f451b64b2d620
Nico <nico.encina2012@gmail.com>
2017-07-04 23:06:39 -0400

    Nuevas funciones para hacer uso de los archivos .mat con caracteristicas guardadas

---
+++ consult_image_v2.m
@@ -0,0 +1,28 @@
+function [rank] = consult_image_v2(image_index, image_class, features_database, features_query, image_database, database_info)
+
+query_features = features_query{image_index};
+
+n_img_database = length(features_database);
+distances = zeros(n_img_database, 1);
+for j=1:n_img_database
+    first_image = features_database{j};
+    dist = chisquare_distance(query_features, first_image);
+    distances(j) = dist;
+end
+
+[B,I] = sort(distances);
+
+Ri_sum = 0;
+N_rel = 0;
+for k=1:length(I)
+    index = I(k);
+    database_image_class = database_info(index).class;
+    
+    if sum(database_image_class == image_class) == 3
+        Ri_sum = Ri_sum + k;
+        N_rel = N_rel + 1;
+    end
+end
+rank = Ri_sum/N_rel;
+
+end
---
+++ rank_iteration.m
@@ -0,0 +1,29 @@
+clear all
+% Abrir imagenes de la carpeta de consulta
+pc_name = 'Nico';
+folder = 'ProyectoFinal';
+
+% Info de cada conjunto de imagenes
+query_database = 'img_query';
+image_database = 'img_database';
+database_info = images_info(pc_name, folder, image_database); 
+query_info = images_info(pc_name, folder, query_database);
+
+n_query = length(query_info);
+
+filenames_database = {'features1','features2','features3','features4'};
+filenames_query = {'features1_query','features2_query','features3_query','features4_query'}; 
+
+% Extraccion de vectores de caracteristicas guardados en archivos
+method = 1;
+db = load(filenames_database{method});
+db_features = db.database_features;
+
+dq = load(filenames_query{method});
+dq_features = dq.database_features;
+
+for k=1:n_query
+   image_name = query_info(k).name;
+   image_class = query_info(k).class;
+   rank = consult_image_v2(k, image_class, db_features, dq_features, image_database, database_info);
+end
